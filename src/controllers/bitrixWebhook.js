41e0fa882a1ac4cfda6e60a6fe2f7b3d282bb341
Svoboda.Gaming <medmish6@gmail.com>
2024-08-08 12:22:24 +0300

    modified:   src/controllers/bitrixWebhook.js

--- src/controllers/bitrixWebhook.js
+++ src/controllers/bitrixWebhook.js
@@ -3,6 +3,12 @@ const handleWebhook = (req, res) => {
     const ip = req.headers['x-forwarded-for'] || req.socket.remoteAddress;
     
     console.log(`Webhook received from: ${ip}`);
+
+    console.log(`Request method: ${req.method}`);
+    console.log(`Request URL: ${req.url}`);
+    console.log('Headers:', req.headers);
+    console.log('Body:', req.body);
+    
     res.status(200).send('Ok');
 
 };
