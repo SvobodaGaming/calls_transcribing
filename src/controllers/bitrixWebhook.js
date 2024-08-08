0f9caf3990e2ad02cc7c2658ef46ef8666836373
Svoboda.Gaming <medmish6@gmail.com>
2024-08-08 15:36:25 +0300

    modified:   package.json 	modified:   src/controllers/bitrixWebhook.js

--- src/controllers/bitrixWebhook.js
+++ src/controllers/bitrixWebhook.js
@@ -1,18 +1,35 @@
-const handleWebhook = (req, res) => {
-    const event = req.body;
-    const ip = req.headers['x-forwarded-for'] || req.socket.remoteAddress;
-    
-    console.log(`Webhook received from: ${ip}`);
+const multer = require('multer');
+const path = require('path');
 
+const storage = multer.diskStorage({
+    destination: (req, file, cb) => {
+        cb(null, 'uploads/');
+    },
+    filename: (req, file, cb) => {
+        cb(null, Date.now() + path.extname(file.originalname));
+    }
+});
+
+const upload = multer({ storage });
+
+const handleWebhook = (req, res) => {
+    const ip = req.headers['x-forwarded-for'] || req.socket.remoteAddress;
+
+    console.log(`Webhook received from: ${ip}`);
     console.log(`Request method: ${req.method}`);
     console.log(`Request URL: ${req.url}`);
     console.log('Headers:', req.headers);
     console.log('Body:', req.body);
-    
-    res.status(200).send('Ok');
 
+    if (req.files && req.files.length > 0) {
+        console.log('Files:', req.files);
+    } else {
+        console.log('No files uploaded');
+    }
+
+    res.status(200).send('Ok');
 };
 
 module.exports = {
-    handleWebhook
+    handleWebhook: upload.any(),
 };
