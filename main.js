064740f1d77ebf44e0a176f24c3c79dc4c2e5e42
Svoboda.Gaming <medmish6@gmail.com>
2024-08-08 15:47:10 +0300

    modified:   main.js

--- main.js
+++ main.js
@@ -1,21 +1,18 @@
 require('dotenv').config();
+const express = require('express');
+const bodyParser = require('body-parser');
 
-const express       = require('express');
-const bodyParser    = require('body-parser');
-const path          = require('path');
-
-const { handleWebhook }     = require('./src/controllers/bitrixWebhook');
-const { transcribeAudio }   = require('./src/routes/speechToText');
-const { manageText }        = require('./src/routes/textManagerAI');
-const { addTextToSheet }    = require('./src/models/tablesManager');
-const { convertAudio } = require('./src/models/convertAudio');
-
+const { handleWebhook } = require('./src/controllers/bitrixWebhook');
 const port = process.env.PORT || 3000;
 
-app = express();
-app.use(bodyParser.json());
+const app = express();
 
-app.use('/webhook', handleWebhook);
+// Используем bodyParser для обработки JSON и URL-кодированных данных
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+// Подключаем обработчик вебхука
+app.post('/webhook', handleWebhook);
 
 app.listen(port, () => {
     console.log(`Server started on port: ${port}`);
