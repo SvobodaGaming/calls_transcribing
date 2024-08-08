1f7cd04b07a0921a3e942927cdf2446fb144cd97
Svoboda.Gaming <medmish6@gmail.com>
2024-08-08 16:35:51 +0300

    modified:   src/controllers/bitrixWebhook.js

--- src/controllers/bitrixWebhook.js
+++ src/controllers/bitrixWebhook.js
@@ -1,44 +1,28 @@
+const fs = require('fs').promises;
 const path = require('path');
-const fs = require('fs');
 
-const uploadDir = path.join(__dirname, '../uploads');
-
-const ensureDirExists = (dir) => {
-    if (!fs.existsSync(dir)) {
-        fs.mkdirSync(dir, { recursive: true });
-    }
-};
-
-const handleWebhook = (req, res) => {
-    const ip = req.headers['x-forwarded-for'] || req.socket.remoteAddress;
-
-    ensureDirExists(uploadDir);
+const handleWebhook = async (req, res) => {
+    console.log('Request file info:', req.file);
 
     if (!req.file) {
         return res.status(400).send('No file uploaded');
     }
 
-    const tempFilePath = path.join(uploadDir, req.file.filename);
-    const originalName = req.file.originalname;
-    const finalFilePath = path.join(uploadDir, originalName);
+    const tempFilePath = path.join(__dirname, '../../uploads', req.file.filename);
+    const finalFilePath = path.join(__dirname, '../../uploads', 'audio.mp3');
 
-    fs.access(tempFilePath, fs.constants.F_OK, (err) => {
-        if (err) {
-            console.error(`File does not exist: ${tempFilePath}`);
-            return res.status(500).send('Error processing file');
-        }
+    console.log(`Temp file path: ${tempFilePath}`);
+    console.log(`Final file path: ${finalFilePath}`);
 
-        fs.rename(tempFilePath, finalFilePath, (err) => {
-            if (err) {
-                console.error('Error renaming file:', err);
-                return res.status(500).send('Error processing file');
-            }
-            console.log(`File saved: ${originalName}`);
-            res.status(200).send('Ok');
-        });
-    });
-
-    console.log(`Webhook received from: ${ip}`);
+    try {
+        await fs.access(tempFilePath);
+        await fs.rename(tempFilePath, finalFilePath);
+        console.log(`File saved: ${finalFilePath}`);
+        res.status(200).send('Ok');
+    } catch (err) {
+        console.error('Error processing file:', err);
+        res.status(500).send('Error processing file');
+    }
 };
 
 module.exports = {
